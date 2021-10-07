76e617a833c7ad2739bcae5c704b7038dc9962ef
CaptainObi <captainobi.mrt@gmail.com>
2021-10-07 11:54:16 +0100

    turned off cors

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,27 +4,28 @@ import { resolvers } from '../../graphql/resolvers';
 import Cors from 'micro-cors';
 import { createContext } from '../../graphql/context';
 
-const cors = Cors();
+//const cors = Cors();
 
 const apolloServer = new ApolloServer({
   schema,
   resolvers,
   context: createContext,
+  introspection: true,
 });
 
 const startServer = apolloServer.start();
 
-export default cors(async function handler(req, res) {
-  if (req.method === 'OPTIONS') {
-    res.end();
-    return false;
-  }
-  await startServer;
+// export default cors(async function handler(req, res) {
+//   if (req.method === 'OPTIONS') {
+//     res.end();
+//     return false;
+//   }
+//   await startServer;
 
-  await apolloServer.createHandler({
-    path: '/api/graphql',
-  })(req, res);
-});
+//   await apolloServer.createHandler({
+//     path: '/api/graphql',
+//   })(req, res);
+// });
 
 export const config = {
   api: {
